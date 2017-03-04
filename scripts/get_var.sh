9932d84309cacf3cfc385a028df60c8e45cced73
Weirong Xu <weirongxu.raidou@gmail.com>
2017-03-04 22:58:41 +0800

    add variables cache

--- scripts/get_var.sh
+++ scripts/get_var.sh
@@ -1,3 +1,9 @@
 #!/usr/bin/env bash
-test -z ${!1} && read -p "Enter your $2: " $1
-test -z ${!1} && exit
+if [ -f cache ]; then
+  . cache
+fi
+if [ -z ${!1} ]; then
+  read -p "Enter your $2: " $1
+  test -z ${!1} && exit
+  echo "$1=${!1}" >> cache
+fi
