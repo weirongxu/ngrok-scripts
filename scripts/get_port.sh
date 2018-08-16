bb01cdc789252999f0469dead80e4578b16c729f
Raidou <weirongxu.raidou@gmail.com>
2018-08-16 21:04:22 +0800

    Update get_port.sh

--- scripts/get_port.sh
+++ scripts/get_port.sh
@@ -1,2 +1,2 @@
-. scripts/get_var.sh "NGROK_HTTP_PORT" "http port"
-. scripts/get_var.sh "NGROK_HTTPS_PORT" "https port"
+. scripts/get_var.sh "NGROK_HTTP_PORT" "http local port"
+. scripts/get_var.sh "NGROK_HTTPS_PORT" "https local port"
